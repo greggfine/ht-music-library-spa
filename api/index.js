b2f4d46e187ee2bca40566f1b1f8a942b423735b
Gregg Fine <bbeez8500@gmail.com>
2020-11-18 10:32:51 -0500

    preparing to deploy to heroku

--- api/index.js
+++ api/index.js
@@ -65,7 +65,7 @@ app.get("/audio/:filename", (req, res) => {
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
